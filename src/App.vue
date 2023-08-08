2108600061c7b207ee8e067deafc3730338a5e72
unknown <erickgutierrez085@gmail.com>
2023-08-07 20:44:59 -0500

    Primeras validaciones hechas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="body">
     <div class="container">
-      <button style="margin: 20px 0px;" data-bs-toggle="modal" data-bs-target="#exampleModal" id="boton">Agregar Producto</button>
+      <button style="margin: 20px 0px;" data-bs-toggle="modal" data-bs-target="#exampleModal" id="boton">Agregar
+        Producto</button>
       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
@@ -13,44 +14,40 @@
             <div class="modal-body">
 
               <h1 class="title">Datos de la tabla</h1>
-              <div class="codigodiv" id="divs">
-                <label>Código</label>
-                <h1 v-text="Error_codigo"></h1>
+              <div class="codigodiv" id="divs" ref="codigodivRef">
+                <label>{{ Error_c }}</label>
                 <input type="text" v-model="Codigo" id="Codigo" class="cajas">
               </div>
-              <div class="nombrediv" id="divs">
-                <label for="Nombre">Nombre</label>
-                <h1 v-text="Error_nombre"></h1>
+              <div class="nombrediv" id="divs" ref="nombredivRef">
+                <label>{{ Error_n }}</label>
                 <input type="text" v-model="Nombre" id="Nombre" class="cajas">
               </div>
 
-              <div class="cantidaddiv" id="divs">
-                <label for="Cantidad">Cantidad</label>
-                <h1 v-text="Error_cantidad"></h1>
+              <div class="cantidaddiv" id="divs" ref="cantidaddivRef">
+                <label for="Cantidad">{{ Error_ca }}</label>
+
                 <input type="number" v-model="Cantidad" id="Cantidad" class="cajas">
               </div>
 
-              <div class="preciodiv" id="divs">
-                <label for="Precio">Precio</label>
-                <h1 v-text="Error_precio"></h1>
+              <div class="preciodiv" id="divs" ref="preciodivRef">
+                <label>{{ Error_p }}</label>
                 <input type="number" v-model="Precio" id="Precio" class="cajas">
               </div>
 
-              <div class="costodiv" id="divs">
+              <div class="costodiv" id="divs" ref="costodivRef">
 
-                <label for="Costo">Costo</label>
-                <h1 v-text="Error_costo"></h1>
+                <label>{{ Error_co }}</label>
+
                 <input type="number" v-model="Costo" id="Costo" class="cajas">
               </div>
 
-             
+
 
             </div>
 
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="boton" >Cerrar</button>
-              <button  type="button" class="btn btn-primary" @click="agregarProducto()"
-                id="boton">{{Boton}}</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="botonm">Cerrar</button>
+              <button type="button" class="btn btn-primary" @click="agregarProducto()" id="botonm">{{ Boton }}</button>
             </div>
           </div>
         </div>
@@ -71,12 +68,12 @@
             <th>Precio</th>
             <th>Costo</th>
             <th>Precio Total</th>
-              <th>Costo Total</th>
-              <th>Ganancias</th>
+            <th>Costo Total</th>
+            <th>Ganancias</th>
             <th id="th9">Opciones</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody class="tbody">
           <tr v-for="(producto, i) in productos" :key="i">
             <td>{{ producto.Codigo }}</td>
             <td>{{ producto.Nombre }}</td>
@@ -85,12 +82,13 @@
               {{ producto.Cantidad }}</td>
             <td>{{ producto.Precio }}</td>
             <td>{{ producto.Costo }}</td>
-            <td>{{ producto.Precio*producto.Cantidad }}</td>
+            <td>{{ producto.Precio * producto.Cantidad }}</td>
             <td>{{ producto.Costo * producto.Cantidad }}</td>
-            <td>{{ producto.Precio * producto.Cantidad-producto.Costo*producto.Cantidad }}</td>
+            <td>{{ producto.Precio * producto.Cantidad - producto.Costo * producto.Cantidad }}</td>
             <td>
-              <button @click="Eliminar(i)"   id="Elibtn">❌</button>
-              <button  @click="Editar(producto, i)"  id="Edibtn" data-bs-toggle="modal" data-bs-target="#exampleModal">✍️</button>
+              <button @click="Eliminar(i)" id="Elibtn">❌</button>
+              <button @click="Editar(producto, i)" id="Edibtn" data-bs-toggle="modal"
+                data-bs-target="#exampleModal">✍️</button>
             </td>
           </tr>
         </tbody>
@@ -104,15 +102,10 @@
 import { ref } from 'vue';
 const Codigo = ref("")
 const Nombre = ref("")
-const Cantidad = ref(0)
-const Precio = ref(0)
-const Costo = ref(0)
-let Error_codigo = ref("")
-let Error_nombre = ref("")
-let Error_cantidad = ref("")
-let Error_precio = ref("")
-let Error_costo = ref("")
-let codigodiv = ref("")
+const Cantidad = ref("")
+const Precio = ref("")
+const Costo = ref("")
+
 
 
 let Boton = ref("Agregar")
@@ -126,41 +119,44 @@ const productos = ref([
 
 
 /* const modalAbierto = ref(false);  */
-let indiceEdicion = ref(-1); 
+let indiceEdicion = ref(-1);
 
 function agregarProducto() {
-  if (Boton.value === "Editar") {
-    if (indiceEdicion.value >= 0) {
-      const productoEditado = {
+  if (validar()) {
+    if (Boton.value === "Editar") {
+      if (indiceEdicion.value >= 0) {
+        const productoEditado = {
+          Codigo: Codigo.value,
+          Nombre: Nombre.value,
+          Cantidad: Cantidad.value,
+          Precio: Precio.value,
+          Costo: Costo.value,
+
+        };
+
+
+        productos.value.splice(indiceEdicion.value, 1, productoEditado);
+        Boton.value = "Agregar";
+        Limpiar();
+        cerrarModal();
+      }
+    } else {
+      const nuevoProducto = {
         Codigo: Codigo.value,
         Nombre: Nombre.value,
         Cantidad: Cantidad.value,
         Precio: Precio.value,
         Costo: Costo.value,
-        
-      };
-      validar();
 
-      productos.value.splice(indiceEdicion.value, 1, productoEditado);
-      Boton.value = "Agregar";
+      };
+
+      productos.value.push(nuevoProducto);
       Limpiar();
       cerrarModal();
     }
-  } else {
-    const nuevoProducto = {
-      Codigo: Codigo.value,
-      Nombre: Nombre.value,
-      Cantidad: Cantidad.value,
-      Precio: Precio.value,
-      Costo: Costo.value,
-      
-    };
-    validar();
-    productos.value.push(nuevoProducto);
-    Limpiar();
-    cerrarModal();
   }
 }
+
 
 function Eliminar(eli) {
   productos.value.splice(eli, 1);
@@ -169,9 +165,9 @@ function Eliminar(eli) {
 function Limpiar() {
   Codigo.value = "";
   Nombre.value = "";
-  Cantidad.value = 0;
-  Precio.value = 0;
-  Costo.value = 0;
+  Cantidad.value = "";
+  Precio.value = "";
+  Costo.value = "";
 }
 
 function Editar(Edi, index) {
@@ -180,22 +176,157 @@ function Editar(Edi, index) {
   Cantidad.value = Edi.Cantidad;
   Precio.value = Edi.Precio;
   Costo.value = Edi.Costo;
-  Boton.value = "Editar";   
-  indiceEdicion.value = index; 
+  Boton.value = "Editar";
+  indiceEdicion.value = index;
 
 }
 
 
-let validation = false
-function validar(){
-  if(Codigo.value.trim()==""){
-    Error_codigo.value ="Error";
-    codigodiv.value.style = "border:1px solid red"
+let codigodivRef = ref();
+let nombredivRef = ref();
+let cantidaddivRef = ref();
+let preciodivRef = ref();
+let costodivRef = ref();
+let Error_c = ref("Codigo")
+let Error_n = ref("Nombre")
+let Error_ca = ref("Cantidad")
+let Error_p = ref("Precio")
+let Error_co = ref("Codigo")
 
 
+
+
+function validar() {
+  let validation = true;
+  if (Codigo.value.trim() == "") {
+    Error_c.value = "Por favor digite el codigo del producto";
+    codigodivRef.value.style.border = "5px solid red";
+    codigodivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+
+    setTimeout(function () {
+      Error_c.value = "Codigo";
+      codigodivRef.value.style = "";
+    }, 2500)
+    validation = false;
   }
+
+  else if (Nombre.value.trim() == "") {
+    Error_n.value = "Por favor digite el nombre del producto";
+    nombredivRef.value.style.border = "5px solid red";
+    nombredivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+
+    setTimeout(function () {
+      Error_n.value = "Nombre";
+      nombredivRef.value.style = "";
+    }, 2500)
+    validation = false;
+  }
+
+
+
+  else if ((isNaN(parseFloat(Cantidad.value)) || parseFloat(Cantidad.value) <= 0)) {
+    Error_ca.value = "Por favor digite la cantidad del producto";
+    cantidaddivRef.value.style.border = "5px solid red";
+    cantidaddivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+    
+    setTimeout(function () {
+      Error_ca.value = "Cantidad";
+      cantidaddivRef.value.style = "";
+    }, 2500)
+    validation = false;
+  }
+
+  else if ((isNaN(parseFloat(Precio.value)) || parseFloat(Precio.value) <= 0)) {
+    Error_p.value = "Por favor digite el precio del producto";
+    preciodivRef.value.style.border = "5px solid red";
+    preciodivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+    
+    setTimeout(function () {
+      Error_p.value = "Precio";
+      preciodivRef.value.style = "";
+    }, 2500)
+    validation = false;
+  }
+
+  else if ((isNaN(parseFloat(Costo.value)) || parseFloat(Costo.value) <= 0)) {
+    Error_co.value = "Por favor digite el costo del producto";
+    costodivRef.value.style.border = "5px solid red";
+    costodivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+    
+    setTimeout(function () {
+      Error_co.value = "Costo";
+      costodivRef.value.style = "";
+    }, 2500)
+    validation = false;
+  }
+  return validation;
 }
 
+/* function validar() {
+  let validation = true
+  if (Codigo.value.trim() == "") {
+    setTimeout(function () {
+      Error_c.value = "Por favor digite el codigo del producto";
+      codigodivRef.value.style.border = "5px solid red";
+      codigodivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+      setTimeout(function () {
+        Error_c.value = "Codigo"
+        codigodivRef.value.style = ""
+      }, 3000)
+    })
+    validation = false
+  }
+  else if (Nombre.value.trim() == "") {
+    setTimeout(function () {
+      Error_n.value = "Por favor digite el nombre del producto";
+      nombredivRef.value.style.border = "5px solid red";
+      nombredivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+      setTimeout(function () {
+        Error_n.value = "Nombre"
+        nombredivRef.value.style = ""
+      }, 3000)
+    })
+    validation = false
+  }
+  else if (Cantidad.value.trim() == "") {
+    setTimeout(function () {
+      Error_ca.value = "Por favor digite la cantidad del producto";
+      cantidaddivRef.value.style.border = "5px solid red";
+      cantidaddivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+      setTimeout(function () {
+        Error_ca.value = "Cantidad"
+        cantidaddivRef.value.style = ""
+      }, 3000)
+    })
+    validation = false
+  }
+  else if (Precio.value.trim() == "") {
+    setTimeout(function () {
+      Error_p.value = "Por favor digite el precio del producto";
+      preciodivRef.value.style.border = "5px solid red";
+      preciodivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+      setTimeout(function () {
+        Error_p.value = "Precio"
+        preciodivRef.value.style = ""
+      }, 3000)
+    })
+    validation = false
+  }
+  else if (Costo.value.trim() == "") {
+    setTimeout(function () {
+      Error_co.value = "Por favor digite el costo del producto";
+      costodivRef.value.style.border = "5px solid red";
+      costodivRef.value.style.backgroundColor = "rgb(255, 179, 179)";
+      setTimeout(function () {
+        Error_co.value = "Costo"
+        costodivRef.value.style = ""
+      }, 3000)
+    })
+    validation = false
+  }
+  return validation
+}
+ */
 </script>
 
 <style scoped>
@@ -204,7 +335,12 @@ function validar(){
   border-radius: 20px;
 
 }
-.modal-body{
+
+.tbody {
+  border-bottom-left-radius: 100px;
+}
+
+.modal-body {
   display: flex;
   flex-direction: column;
   gap: 15px;
@@ -228,30 +364,36 @@ function validar(){
   width: 1400px;
 }
 
-#Edibtn:hover{
+#Edibtn:hover {
   background-color: rgb(90, 255, 178);
-  transform:   scale(1.1);
-  border: 3px solid green; 
+  transform: scale(1.1);
+  border: 3px solid green;
 }
 
-#Elibtn:hover{
+#Elibtn:hover {
   background-color: rgb(255, 179, 179);
-  transform:   scale(1.1);
-  border: 3px solid red; 
+  transform: scale(1.1);
+  border: 3px solid red;
+  
 }
-#Elibtn{
- border: none;
+
+#Elibtn {
+  border: none;
   background-color: rgb(255, 215, 215);
+  border-radius: 10px;
 }
-#Edibtn{
+
+#Edibtn {
   border: none;
   background-color: rgb(133, 255, 200);
+  border-radius: 10px;
 }
 
-#th1{
+#th1 {
   border-top-left-radius: 25px;
 }
-#th9{
+
+#th9 {
   border-top-right-radius: 25px;
 
 }
@@ -286,26 +428,43 @@ td {
   padding: 15px;
   font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   border-radius: 10px;
- border: none;
-  font-family: Arial, Helvetica, sans-serif;
+  border: none;
   font-size: 25px;
   background-color: rgb(184, 228, 255);
   cursor: pointer;
   color: black;
 }
+#botonm{
+  font-weight: bold;
+  padding: 7px;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  border-radius: 10px;
+  border: none;
+  font-size: 15px;
+  background-color: rgb(184, 228, 255);
+  cursor: pointer;
+  color: black;
+}
+
 tr:nth-child(even) {
-  background-color: rgb(184, 228, 255); /* Color para las filas impares */
+  background-color: rgb(184, 228, 255);
+  /* Color para las filas impares */
+}
+#botonm:hover{
+  background-color: rgb(135, 211, 255);
+  transform: scale(1.1);
+  border: 3px rgb(0, 136, 255) solid;
 }
 #boton:hover {
   background-color: rgb(135, 211, 255);
-  transform:   scale(1.1);
+  transform: scale(1.1);
   border: 3px rgb(0, 136, 255) solid;
 
 }
 
 th {
   background-color: rgb(113, 189, 255);
-  padding: 10px;
+  padding: 20px;
   text-align: center;
 
 }
@@ -325,4 +484,5 @@ th {
 button {
   margin: 5px;
   padding: 7px;
-}</style>
+}
+</style>
